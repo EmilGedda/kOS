0dc2e63d2d170e25f61d7455dc9133cf70dbd6b7
Emil Gedda <emil.gedda@emilgedda.se>
2018-08-31 12:32:00 +0200

    more type traits

--- tests/std/main.cpp
+++ tests/std/main.cpp
@@ -1,5 +1,8 @@
 #include <type_traits>
 
+using namespace std;
 int main() {
-  static_assert(std::is_same_v<int, int>);
+  static_assert(is_same_v<int, int>);
+  static_assert(is_same_v<conditional_t<true, int, double>, int>);
+  static_assert(is_same_v<remove_cvref_t<volatile int const&&>, int>);
 }
